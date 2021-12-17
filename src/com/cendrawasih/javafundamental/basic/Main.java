21788a02b864d71e013c3095a88c5b98bf874fb2
itdag [Irfan.Nawawi] <0446511018@bussan.co.id>
2021-12-17 10:20:35 +0700

    Learn Struktur Data

--- src/com/cendrawasih/javafundamental/basic/Main.java
+++ src/com/cendrawasih/javafundamental/basic/Main.java
@@ -1,7 +1,29 @@
 package com.cendrawasih.javafundamental.basic;
 
+import com.cendrawasih.javafundamental.basic.kendaraan.Kereta;
+import com.cendrawasih.javafundamental.basic.kendaraan.Mobil;
+import com.cendrawasih.javafundamental.basic.kendaraan.Motor;
+import com.cendrawasih.javafundamental.basic.musik.Gitar;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Date;
+
 public class Main {
     public static void main (String[] args) {
         System.out.println("Hello World !");
+
+        // musik
+        Gitar.bunyi();
+
+        // kendaraan
+        Mobil.jumlahBan();
+        Motor.jumlahBan();
+        Kereta.jumlahBan();
+
+        // lib
+        Date today = new Date();
+        System.out.println("Hari ini = "+ today);
+        Date tomorrow = DateUtils.addDays(today, 1);
+        System.out.println("Besok = "+ tomorrow);
     }
 }
---
+++ src/com/cendrawasih/javafundamental/basic/musik/Gitar.java
@@ -0,0 +1,7 @@
+package com.cendrawasih.javafundamental.basic.musik;
+
+public class Gitar {
+    public static void bunyi () {
+        System.out.println("Jreeengg...");
+    }
+}

b8250ca943d408ffab9a61d6e3aa4863991ac253
itdag [Irfan.Nawawi] <0446511018@bussan.co.id>
2021-12-26 10:47:12 +0700

    Learn Percabangan

---
+++ src/com/cendrawasih/javafundamental/basic/percabangan/IfThen.java
@@ -0,0 +1,10 @@
+package com.cendrawasih.javafundamental.basic.percabangan;
+
+public class IfThen {
+    public static void main(String[] args) {
+        boolean isOn = true;
+
+        if (isOn) System.out.println("Menyalakan lampu");
+        System.out.println("Menyalakan lampu lain");
+    }
+}

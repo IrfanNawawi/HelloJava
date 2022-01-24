92c252953ad641dc9fb2ad86ce33d28e2b72b9e1
IrfanNawawi <irfnawi@gmail.com>
2022-01-25 05:50:41 +0700

    Learn DateTime

---
+++ src/com/cendrawasih/javafundamental/basic/datetime/ExampleDate.java
@@ -0,0 +1,10 @@
+package com.cendrawasih.javafundamental.basic.datetime;
+
+import java.util.Date;
+
+public class ExampleDate {
+    public static void main (String[] args) {
+        Date date = new Date();
+        System.out.println("Tanggal sekarang adalah "+date.toString());
+    }
+}
